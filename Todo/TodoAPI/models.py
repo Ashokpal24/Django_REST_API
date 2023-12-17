d32f87d4ad66f07a304acccf3d6d2bc9fc1a2ee9
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-12-17 07:42:26 +0000

    Added test.

--- Todo/TodoAPI/models.py
+++ Todo/TodoAPI/models.py
@@ -7,13 +7,22 @@ class UserProfile(models.Model):
     email=models.EmailField()
     created_at=models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 class Subtask(models.Model):
     title = models.CharField(max_length=255, blank=False)
     complete = models.BooleanField(default=False)
     task = models.ForeignKey('Task', related_name='subtasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+    
 
 class Task(models.Model):
     title = models.CharField(max_length=255,blank=False)
     percentage = models.IntegerField(default=0)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     # subtasks = JSONField(default=list)
+    def __str__(self):
+        return '{}'.format(self.title)
--- Todo/TodoAPI/tests.py
+++ Todo/TodoAPI/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from urllib import response
+from .models import UserProfile
+from rest_framework.test import APIClient,APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class UserTestCase(APITestCase):
+    def setUp(self):
+        self.client=APIClient()
+        self.data=[
+            {
+                "name":"Jaz",
+                "email":"Jaz11greed@gmail.com"
+            },
+            {
+                "name":"Jade",
+                "email":"Jade77gasper@gmail.com"
+            }
+        ]
+        self.url="/api/"
+
+
+    def test_create_user(self):
+        for new_data in self.data:
+            data=new_data
+            response=self.client.post(self.url,data)
+            self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(UserProfile.objects.count(),2)
+        self.assertEqual(UserProfile.objects.get(id=1).name,"Jaz")
+
+    def test_get_user(self):
+        response=self.client.get(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    
